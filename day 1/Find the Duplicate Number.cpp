bfa6b4c45f4bbf1799246d4b8451ca782028072f
sweta thakur <99349185+sweta-thakur112@users.noreply.github.com>
2022-05-16 11:48:45 +0530

    Update Find the Duplicate Number.cpp

--- day 1/Find the Duplicate Number.cpp
+++ day 1/Find the Duplicate Number.cpp
@@ -1,1 +1,20 @@
 
+```
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            if(nums[i]==nums[i+1])
+            {
+                n=nums[i];
+                break;
+            }
+      }
+        return n;
+    }
+};
+```
+//status - completed
