0faeb50916ec0f63423f2dbdfcbaf411b57536f6
sweta thakur <99349185+sweta-thakur112@users.noreply.github.com>
2022-05-19 13:34:30 +0530

    Create maximum product of three numbers.cpp

---
+++ day 3/maximum product of three numbers.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        sort(nums.begin(),nums.end()); 
+        int n=nums.size()-1;
+        int maxu=max(nums[n]*nums[n-1]*nums[n-2],nums[0]*nums[1]*nums[n]);
+        return maxu;
+    }
+};
