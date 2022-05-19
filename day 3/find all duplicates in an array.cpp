ecd23dfe1089b18cd1bd680b2cd2fa02b38e89ad
sweta thakur <99349185+sweta-thakur112@users.noreply.github.com>
2022-05-19 11:45:12 +0530

    Update find all duplicates in an array.cpp

--- day 3/find all duplicates in an array.cpp
+++ day 3/find all duplicates in an array.cpp
@@ -1,1 +1,13 @@
-//
+ class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        map<int,int>m;
+        vector<int>v;
+        for(int i=0;i<nums.size();i++)
+            m[nums[i]]++;
+        for(auto it:m)
+            if(it.second>1)
+                v.push_back(it.first);
+        return v;
+    }
+};
