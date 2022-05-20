1a86ac760e554e9a1bea90f990080c4559e9b93b
sweta thakur <99349185+sweta-thakur112@users.noreply.github.com>
2022-05-20 23:21:36 +0530

    Update happy number.cpp

--- day 4/happy number.cpp
+++ day 4/happy number.cpp
@@ -1,1 +1,19 @@
-// pending
+class Solution {
+public:
+    unordered_set<int> seenSum;
+    bool isHappy(int n) {
+        if(n==1){
+            return true;
+        }else{
+            int sum = sumSD(n);
+            if(seenSum.count(sum)){
+                return false;
+            }
+            seenSum.insert(sum);
+            return isHappy(sum);
+        }
+    }
+    int sumSD(int num){
+        return num==0 ? 0 : ((num%10) * (num%10)) + sumSD(num/10);
+    }
+};
